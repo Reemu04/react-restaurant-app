6da2edfc183f977b642e6391508ee9dda94084f5
“Reemu04” <reemu.glsica17@gmail.com>
2022-08-13 16:51:42 +0530

    mobile View and logout functinality

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -29,9 +29,17 @@ const Header = () => {
       setIsMenu(!isMenu);
     }
   };
-  // const url = user.photoURL;
+
+  const logout = () => {
+    setIsMenu(false);
+    localStorage.clear();
+    dispatch({
+      type: actionType.SET_USER,
+      user: null,
+    });
+  };
   return (
-    <header className=" fixed z-50 w-screen  p-6 px-16">
+    <header className=" fixed z-50 w-screen  p-3 px-4 md:p-6 md:px-16">
       {/* desktop and tablet */}
       <div className="hidden md:flex w-full h-full items-center justify-between">
         <Link to="/" className="flex items-center gap-2">
@@ -39,7 +47,12 @@ const Header = () => {
           <p className="text-headingColor text-x1 font-bold">City</p>
         </Link>
         <div className="flex items-center  gap-8">
-          <ul className="flex items-center gap-8 ">
+          <motion.ul
+            initial={{ opacity: 0, x: 200 }}
+            animate={{ opacity: 1, x: 0 }}
+            exit={{ opacity: 0, x: 200 }}
+            className="flex items-center gap-8 "
+          >
             <Link to="/">
               <li className="text-base text-textColor hover:text-headingColor duration-100 transition-all ease-in-out cursor-pointer">
                 Home
@@ -54,7 +67,7 @@ const Header = () => {
             <li className="text-base text-textColor hover:text-headingColor duration-100 transition-all ease-in-out cursor-pointer">
               Services
             </li>
-          </ul>
+          </motion.ul>
 
           <div className="relative flex items-center justify-center">
             <MdShoppingBasket className="text-textColor text-2xl  cursor-pointer" />
@@ -91,7 +104,10 @@ const Header = () => {
                   </Link>
                 )}
 
-                <p className="px-4 py-2 flex items-center gap-3 cursor-pointer hover:bg-slate-100 transition-all duration-100 ease-in-out text-textColor text-base">
+                <p
+                  className="px-4 py-2 flex items-center gap-3 cursor-pointer hover:bg-slate-100 transition-all duration-100 ease-in-out text-textColor text-base"
+                  onClick={logout}
+                >
                   Logout
                   <MdLogout />{" "}
                 </p>
@@ -101,7 +117,65 @@ const Header = () => {
         </div>
       </div>
       {/* mobile */}
-      <div className="flex md:hidden w-full h-full"></div>
+      <div className="flex items-center justify-between md:hidden w-full h-full">
+        <Link to="/" className="flex items-center gap-2">
+          <img src={Logo} alt="logo" className="w-8 object-cover " />
+          <p className="text-headingColor text-x1 font-bold">City</p>
+        </Link>
+        <div className="relative">
+          <motion.img
+            whileTap={{ scale: 0.6 }}
+            src={
+              user ? user.photoURL || firebaseAuth.currentUser.photoURL : Avatar
+            }
+            alt="userProfile"
+            className="w-10 min-w-[40px] h-10 min-h-[40px] drop-shadow-xl cursor-pointer rounded-full"
+            onClick={login}
+          />
+          {isMenu && (
+            <motion.div
+              initial={{ opacity: 0, scale: 0.6 }}
+              animate={{ opacity: 1, scale: 1 }}
+              exit={{ opacity: 0, scale: 0.6 }}
+              className=" w-40 bg-gray-50 shadow-xl rounded-lg flex flex-col absolute top-12 right-0 "
+            >
+              {user && user.email === "reemuiviewlabs@gmail.com" && (
+                <Link to="/createItem">
+                  <p className="px-4 py-2 flex items-center gap-3 cursor-pointer hover:bg-slate-100 transition-all duration-100 ease-in-out text-textColor text-base">
+                    New Item
+                    <MdAdd />{" "}
+                  </p>
+                </Link>
+              )}
+
+              <ul className="flex flex-col ">
+                <Link to="/">
+                  <li className="text-base text-textColor hover:text-headingColor duration-100 transition-all ease-in-out cursor-pointer hover:bg-slate-100 px-4 py-2 ">
+                    Home
+                  </li>
+                </Link>
+                <li className="text-base text-textColor hover:text-headingColor duration-100 transition-all ease-in-out cursor-pointer hover:bg-slate-100 px-4 py-2">
+                  Menu
+                </li>
+                <li className="text-base text-textColor hover:text-headingColor duration-100 transition-all ease-in-out cursor-pointer hover:bg-slate-100 px-4 py-2">
+                  About Us
+                </li>
+                <li className="text-base text-textColor hover:text-headingColor duration-100 transition-all ease-in-out cursor-pointer hover:bg-slate-100 px-4 py-2">
+                  Services
+                </li>
+              </ul>
+
+              <p
+                className="m-2 p-2 rounded-md shadow-md flex items-center justify-center bg-gray-200 gap-3 cursor-pointer hover:bg-gray-300 transition-all duration-100 ease-in-out text-textColor text-base"
+                onClick={logout}
+              >
+                Logout
+                <MdLogout />{" "}
+              </p>
+            </motion.div>
+          )}
+        </div>
+      </div>
     </header>
   );
 };
