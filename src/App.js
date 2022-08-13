6dafebc91698cf0603ccf49e3a4f8c2751a2e926
“Reemu04” <reemu.glsica17@gmail.com>
2022-08-13 10:33:29 +0530

    Header component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
+import { AnimatePresence } from "framer-motion";
 import React from "react";
+import { Route, Routes } from "react-router-dom";
 import { Header } from "./components";
-
+import { CreateContainer } from "./components";
+import { MainContainer } from "./components";
 const App = () => {
   return (
-    <div className="w-screen h-auto flex flex-col ">
-      <Header />
-    </div>
+    <AnimatePresence>
+      <div className="w-screen h-auto flex flex-col bg-primary">
+        <Header />
+        <main className="mt-24 p-8 w-full">
+          <Routes>
+            <Route path="/" element={<MainContainer />} />
+            <Route path="/createItem" element={<CreateContainer />} />
+          </Routes>
+        </main>
+      </div>
+    </AnimatePresence>
   );
 };
 
